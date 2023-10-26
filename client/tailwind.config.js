4c7dde2498fe03bf5766c6d7a3bab0507002829a
sravan <sravantp03@gmail.com>
2023-10-26 10:42:37 +0530

    added header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F5385D', // Adding custom color
+      },
+    },
   },
   plugins: [],
 }
