93cdc73f2792b0c03d1043d577ff49ba087ebbc0
sravan <sravantp03@gmail.com>
2023-10-26 14:42:09 +0530

    added registration page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom'
+
 function Register() {
-  return <div>Register</div>
+  return (
+    <div className="mt-20">
+      <h1 className="text-3xl text-center">Register</h1>
+      <form className="mt-2 max-w-md mx-auto">
+        <input type="text" placeholder="John Doe" />
+        <input type="email" placeholder="your@email.com" />
+        <input type="password" placeholder="password" />
+        <button className="primary">Register</button>
+        <div className="mt-2 text-center">
+          <span className="text-gray-500">Already a member? </span>
+          <Link to={'/login'} className="text-primary underline">
+            Login
+          </Link>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Register
