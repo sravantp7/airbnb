de9704ccc9e1a54fb213c12a2b25e1bb98ef580c
sravan <sravantp03@gmail.com>
2023-10-27 14:25:58 +0530

    added mongodb + created api for registering user

--- server/index.js
+++ server/index.js
@@ -1,13 +1,53 @@
 const express = require('express')
 const app = express()
-const PORT = 5000
 const cors = require('cors')
+const mongoose = require('mongoose')
+const PORT = 5000
+require('dotenv').config()
+const bcrypt = require('bcryptjs') // for password encryption
 
-app.use(cors())
+const bcryptSalt = bcrypt.genSaltSync(10)
+
+// models
+const User = require('./models/User') // importing UserModel
+
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+  })
+)
+
 app.use(express.json())
 
-app.get('/api/test', (req, res) => {
-  res.send('Test Ok')
+// Connection
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING)
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(`Error - ${err.message}`))
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Airbnb API')
+})
+
+app.post('/api/register', async (req, res) => {
+  const { name, email, password } = req.body
+
+  try {
+    // creating a new user
+    const user = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt), // password encryption
+    })
+    res.send(user)
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+})
+
+app.get('*', (req, res) => {
+  res.send('Invalid Request')
 })
 
 app.listen(PORT, () => {
---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+// schema
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+// model
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = UserModel
