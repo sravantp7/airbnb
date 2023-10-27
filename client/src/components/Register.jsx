301a3256aa54bb01d983ef8f4c93d4348c5cb6d3
sravan <sravantp03@gmail.com>
2023-10-27 14:46:59 +0530

    registration fix

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,13 +1,58 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+const SERVER_URL = 'http://127.0.0.1:5000'
 
 function Register() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function registerUser(e) {
+    e.preventDefault()
+
+    try {
+      const res = await axios.post(`${SERVER_URL}/api/register`, {
+        name,
+        email,
+        password,
+      })
+
+      if (res.data.email) {
+        alert('Registration Successful')
+      }
+    } catch (err) {
+      if (err.response.data.error) {
+        alert('Registration Failed, Email has already registered')
+      } else {
+        alert('Something went wrong, Please try again later')
+      }
+    }
+  }
+
   return (
     <div className="mt-20">
       <h1 className="text-3xl text-center">Register</h1>
-      <form className="mt-2 max-w-md mx-auto">
-        <input type="text" placeholder="John Doe" />
-        <input type="email" placeholder="your@email.com" />
-        <input type="password" placeholder="password" />
+      <form className="mt-2 max-w-md mx-auto" onSubmit={registerUser}>
+        <input
+          type="text"
+          placeholder="John Doe"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="your@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button className="primary">Register</button>
         <div className="mt-2 text-center">
           <span className="text-gray-500">Already a member? </span>
