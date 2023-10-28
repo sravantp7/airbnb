7161694147e4c158c2c11cc2dc4c093bee80bc5e
sravan <sravantp03@gmail.com>
2023-10-28 13:34:22 +0530

    added jwt token + fix

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const PORT = 5000
 require('dotenv').config()
 const bcrypt = require('bcryptjs') // for password encryption
+const jwt = require('jsonwebtoken')
 
 const bcryptSalt = bcrypt.genSaltSync(10)
 
@@ -31,34 +32,64 @@ app.get('/', (req, res) => {
 })
 
 app.post('/api/register', async (req, res) => {
-  const { name, email, password } = req.body
-
   try {
-    // creating a new user
-    const user = await User.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, bcryptSalt), // password encryption
-    })
-    res.send(user)
+    const { name, email, password } = req.body
+
+    // verifying inputs
+    if (!(name && email && password)) {
+      res.status(422).json({
+        message: 'Registration failed, Please provide name , email & password',
+      })
+    } else {
+      // creating a new user
+      const user = await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcryptSalt), // password encryption
+      })
+
+      user.password = undefined // setting password inside user to undefined
+      res.json(user) // sending entire user data to client
+    }
   } catch (err) {
     res.status(422).json({ error: err.message })
   }
 })
 
 app.post('/api/login', async (req, res) => {
-  const { email, password } = req.body
-
   try {
-    // fetching user details from db
+    const { email, password } = req.body
+
+    // validating  user inputs
+    if (!(email && password)) {
+      res.status(422).json({
+        isLoggedIn: false,
+        message: 'Login Failed, Please provide email & password',
+      })
+    }
+
+    // fetching user details from db using email address
     const user = await User.findOne({
       email,
     })
+
+    // Authenticating user
     if (user) {
       // comparing password used for login with registered encrypted password
       const passwordOk = bcrypt.compareSync(password, user.password)
       if (passwordOk) {
-        res.json({ isLoggedIn: true, message: 'Successfully LoggedIn' })
+        // Creating a JWT token
+        const jwtToken = jwt.sign(
+          { id: user._id, name: user.name, email: user.email }, // payload
+          process.env.JWT_SECRET_TOKEN, // secret key
+          { expiresIn: '1h' } // adding expire time to token
+        )
+
+        res.status(200).json({
+          isLoggedIn: true,
+          message: 'Successfully LoggedIn',
+          jwtToken,
+        })
       } else {
         res.status(422).json({
           isLoggedIn: false,
