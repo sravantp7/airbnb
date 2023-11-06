29200dd861127e07ba1990a47f2f1eec0bd0654e
sravan <sravantp03@gmail.com>
2023-11-06 15:49:31 +0530

    added star button to select main button

--- client/src/components/PlacesPage.jsx
+++ client/src/components/PlacesPage.jsx
@@ -23,6 +23,8 @@ function PlacesPage() {
   const [maxGuests, setMaxGuests] = useState(1)
   const [places, setPlaces] = useState([]) // for holding added places by the user when loading in
 
+  const [redirect, setRedirect] = useState(false)
+
   // function that sends the image url to server
   async function addPhotoByLink(e) {
     e.preventDefault()
@@ -103,19 +105,28 @@ function PlacesPage() {
     if (id) {
       data.id = id // if id available then adding it with the data object
       // update the existing place details (PUT)
-      await axios.put(`${SERVER_URL}/api/places`, data)
+      const res = await axios.put(`${SERVER_URL}/api/places`, data)
     } else {
       // add new place to the db
       // sending place data to the server
-      await axios.post(`${SERVER_URL}/api/places`, data)
+      const res = await axios.post(`${SERVER_URL}/api/places`, data)
     }
+
+    setRedirect(true)
   }
 
-  console.log(addedPhotos)
-
+  // this function removed the uploaded photo, after that pressing save is required
   async function removePhoto(e, filename) {
-    console.log(filename)
+    e.preventDefault()
     setAddedPhotos((prev) => prev.filter((file) => file != filename))
+  }
+
+  // function used to select the main photo out of all uploaded photos by clicking the star icon
+  function setAsMainPhoto(e, photo) {
+    e.preventDefault()
+    console.log('selected photo ' + photo)
+    // changing photo order
+    setAddedPhotos((prev) => [photo, ...prev.filter((file) => file != photo)])
   }
 
   // when clicking on a specific place it will send a request to server with id to get its all details
@@ -146,6 +157,10 @@ function PlacesPage() {
     }
     loadPlaces()
   }, [])
+
+  if (redirect) {
+    return <Navigate to={'/account'} />
+  }
 
   return (
     <div>
@@ -268,6 +283,27 @@ function PlacesPage() {
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+
+                    {/* adding start icon button to select main photo */}
+                    <button
+                      onClick={(e) => setAsMainPhoto(e, link)}
+                      className="absolute bottom-1 left-1 cursor-pointer bg-black p-1 bg-opacity-50 rounded-xl"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-white"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
                         />
                       </svg>
                     </button>
