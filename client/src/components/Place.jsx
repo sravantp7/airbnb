78ad12c6f518b4fc88f0d9456ef104c818ee2373
sravan <sravantp03@gmail.com>
2023-11-07 12:11:11 +0530

    now user can view individual places with extra details by clicking on them

--- client/src/components/Place.jsx
+++ client/src/components/Place.jsx
@@ -7,6 +7,7 @@ const SERVER_URL = import.meta.env.VITE_SERVER_URL
 function Place() {
   const { id } = useParams() // getting the id of the place
   const [place, setPlace] = useState({})
+  const [showAllPhotos, setShowAllPhotos] = useState(false)
 
   useEffect(() => {
     if (!id) {
@@ -23,13 +24,48 @@ function Place() {
     fetchPlaceDetails()
   }, [id])
 
+  // rendering all photos
+  if (showAllPhotos) {
+    return (
+      <div className="min-w-full min-h-screen bg-white relative">
+        <button
+          className="absolute right-[20%] top-6 rounded-full p-2"
+          onClick={() => setShowAllPhotos(false)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <div className="mt-20 flex flex-col gap-4 items-center">
+          {place?.photos?.length > 0 &&
+            place.photos.map((photo) => (
+              <img
+                key={photo}
+                src={`${SERVER_URL}/uploads/${photo}`}
+                className="w-[50%]"
+              />
+            ))}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="mt-4 bg-gray-100 py-6 px-8">
       {place && (
         <div>
           <h1 className="text-2xl mb-2 font-medium">{place.title}</h1>
           <div className="flex items-center gap-2">
-            <p className="flex items-center gap-1">
+            <p className="flex items-center gap-1 text-sm font-medium">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -44,7 +80,7 @@ function Place() {
               </svg>
               4.6
             </p>
-            <p className="text-sm font-medium underline">100 reviews</p>
+            <p className="text-sm font-medium underline">101 reviews</p>
             <a
               href={`https://maps.google.com/?q=${place.address}`}
               target="_blank"
@@ -53,6 +89,8 @@ function Place() {
               {place.address}
             </a>
           </div>
+
+          {/* Photos */}
           <div className="grid gap-2 grid-cols-[2fr_1fr] mt-4">
             <div>
               {place.photos?.[0] && (
@@ -61,6 +99,38 @@ function Place() {
                   alt="img"
                 />
               )}
+            </div>
+            <div className="grid gap-2 relative">
+              <div>
+                {place.photos?.[1] && (
+                  <img
+                    src={`${SERVER_URL}/uploads/${place.photos[1]}`}
+                    alt="img"
+                  />
+                )}
+              </div>
+              <div>
+                {place.photos?.[2] && (
+                  <img
+                    src={`${SERVER_URL}/uploads/${place.photos[2]}`}
+                    alt="img"
+                  />
+                )}
+              </div>
+              <button
+                className="absolute bottom-2 right-2 px-3 py-1 rounded-2xl bg-gray-200 cursor-pointer flex gap-1 shadow"
+                onClick={() => setShowAllPhotos(true)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
+                </svg>
+                <span>Show all photos</span>
+              </button>
             </div>
           </div>
         </div>
